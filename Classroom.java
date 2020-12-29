4e86c70feaaea62659826cd88fd3efc0c93f2e81
mdstudentnew <74984449+mdstudentnew@users.noreply.github.com>
2020-12-29 10:46:36 -0600

    Update Classroom.java

--- Classroom.java
+++ Classroom.java
@@ -1,1 +1,64 @@
 
+
+public class Classroom{
+  private String name = "";
+  private String students= "";
+  private int num_students;
+  private final int MAX_STUDENTS = 30; 
+
+  public Classroom(String theName){
+    name = theName;
+  }
+
+  public String getName(){
+    return name;
+  }
+
+  public void setName(String theName){
+    name = theName;
+  }
+
+  public int getNumStudents(){
+    return num_students; 
+  }
+
+  public void addStudent(String theStudent){
+    if(num_students<MAX_STUDENTS){
+      students = students + " " + theStudent;
+      num_students++;
+      students = students.trim();
+    }
+  }
+ 
+ public String toString(){
+   return students;
+ }
+
+public boolean removeStudent(String theName){
+  int studentIndex = students.indexOf(theName);
+
+  if(studentIndex == -1)
+    return false;
+  else{
+    int len = theName.length();
+    int spaceIndex = studentIndex + len;
+    if(spaceIndex < students.length() &&  students.substring(spaceIndex, spaceIndex+1).equals(" "))
+      len++;
+
+    students = students.substring(0,studentIndex)+students.substring(studentIndex+len);
+    students = students.trim();
+    num_students--;
+    return true;
+
+  }
+  
+}
+ 
+
+
+
+
+
+
+
+}
