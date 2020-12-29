2c5c8be439d3037628accf8355be7164cb5753ff
mdstudentnew <74984449+mdstudentnew@users.noreply.github.com>
2020-12-29 10:47:26 -0600

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,1 +1,14 @@
 
+public class Sample
+{
+
+    public  Classroom create()
+    {
+        Classroom c = new Classroom("Aronson");
+        c.addStudent("Sam");
+        c.addStudent("Sara");
+        c.addStudent("Nate");
+        return c;
+
+    }
+}
